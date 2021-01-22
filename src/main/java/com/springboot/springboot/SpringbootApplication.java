8efdc1c79d1e1728a147df490c49fadc0d0d171f
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-01-22 09:41:01 -0300

    criptografia em memória

--- src/main/java/com/springboot/springboot/SpringbootApplication.java
+++ src/main/java/com/springboot/springboot/SpringbootApplication.java
@@ -6,17 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(scanBasePackages = "com.springboot.springboot.*")
-@EntityScan("com.springboot.springboot.model")
-@EnableJpaRepositories(basePackages = {"com.springboot.springboot.Repository"})
-@EnableTransactionManagement
-@ComponentScan(basePackageClasses = ProdutoController.class)
+@SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootApplication.class, args);
+
+		/*
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String result = encoder.encode("123");
+		System.out.println(result);
+
+		 */
 	}
 
 }
--- src/main/java/com/springboot/springboot/security/WebConfigSecurity.java
+++ src/main/java/com/springboot/springboot/security/WebConfigSecurity.java
@@ -1,39 +1,21 @@
 package com.springboot.springboot.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
+
 @EnableWebSecurity
 public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
-
-    //@Bean
-    //public PasswordEncoder passwordEncoder(){ return new BCryptPasswordEncoder(); }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-                .antMatchers("/categorias").permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic()
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().disable();
-
-        /*
         http.csrf()
                 .disable()
                 .authorizeRequests()
@@ -42,20 +24,18 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
                 .and().formLogin().permitAll()
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-
-         */
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
                 .withUser("thone")
-                .password("123")
+                .password("$2a$10$WULDfqtHFa8pJg5i3RzxOOSef.LgUcoagoHqka/hLh.kAQPxKSnEu")
                 .roles("ADMIN");
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+        web.ignoring().antMatchers("/resources/**");
     }
 }
