b6b678fe5cf5422ae32cba7d592e381ec07dd000
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-03-02 16:32:48 -0300

    conclusão relatório

--- src/main/java/com/springboot/springboot/Repository/PRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.springboot.Repository;
-
-import com.springboot.springboot.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PRepository extends JpaRepository<Pessoa, Long> {
-}
